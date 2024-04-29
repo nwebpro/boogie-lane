5a0001920b7dd011f21238f3731ef5a9fa4a16ec
nwebpro <abnaeem.bsc@gmail.com>
2024-04-29 15:59:57 +0600

    Refactor audio player creation and event listeners in main.js and index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,32 +20,5 @@ $(function () {
 		},
 	});
 
-	// Create the audio player
-	setTimeout(function () {
-		let audioPlayer = document.createElement('audio');
-		audioPlayer.src = 'https://nashe1.hostingradio.ru:18000/jazz-128.mp3';
-		audioPlayer.autoplay = true;
-
-		// Add 'playing' class to the vinyl image when audio starts playing
-		audioPlayer.addEventListener('play', function () {
-			const vinylImage = document.querySelector('.vinel_wrap img');
-			vinylImage.classList.add('playing');
-		});
-
-		// Remove 'playing' class from the vinyl image when audio is paused
-		audioPlayer.addEventListener('pause', function () {
-			const vinylImage = document.querySelector('.vinel_wrap img');
-			vinylImage.classList.remove('playing');
-		});
-
-		// Play/Pause button functionality
-		const playPauseButton = document.getElementById('playPauseButton');
-		playPauseButton.addEventListener('click', function () {
-			if (audioPlayer.paused) {
-				audioPlayer.play();
-			} else {
-				audioPlayer.pause();
-			}
-		});
-	}, timeOutValue);
+	
 });
