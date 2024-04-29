73d5eb4f4067149d7ca171d4dc129a4d0c4a1330
nwebpro <abnaeem.bsc@gmail.com>
2024-04-29 14:27:54 +0600

    Update audio playlist and vinyl image in main.js and index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,15 @@
 $(function () {
 	'use strict';
 
+	const timeOutValue = 5000;
+
 	// Function to show the splash screen after 10 seconds
 	setTimeout(function () {
 		$('.slash_screen_wrapper').fadeOut(400, function () {
 			$(this).remove();
 			$('main').fadeIn(400);
 		});
-	}, 5000);
+	}, timeOutValue);
 
 	// Swiper Slider
 	let heroSwiper = new Swiper('.mySwiper', {
@@ -17,4 +19,31 @@ $(function () {
 			delay: 4000,
 		},
 	});
+
+	function togglePlay() {
+		const audio = document.getElementById('audioPlayer');
+		const vinylImage = document.querySelector('.vinel_wrap img');
+		if (audio.paused) {
+			audio.play();
+			vinylImage.classList.add('playing');
+		} else {
+			audio.pause();
+			vinylImage.classList.remove('playing');
+		}
+	}
+
+	// Event listener to toggle play/pause when the button is clicked
+	document
+		.getElementById('playPauseButton')
+		.addEventListener('click', function () {
+			togglePlay();
+		});
+
+	// Autoplay after 5 seconds
+	setTimeout(function () {
+		document
+			.getElementById('audioPlayer')
+			.setAttribute('autoplay', 'autoplay');
+		togglePlay(); // Start playing the audio
+	}, timeOutValue);
 });
